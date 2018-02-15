1c877fdc2537967d4c59b46674ced795342dd222
EVGENIY SEMENOV <semenovem@gmail.com>
2018-02-15 22:33:16 +0300

    keep

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,36 @@
 const gulp = require('gulp');
 const cleanCSS = require('gulp-clean-css');
 const htmlmin = require('gulp-htmlmin');
+const del = require('del');
 
-const DIR_SOURCE = '_site';
+const SOURCE = '_site';
+const TARGET = '_dist';
 
-gulp.task('build', ['minify_css', 'minify_html'], () => {
+gulp.task('build', ['clean-target', 'minify-css', 'minify-html', 'transfer-resources']);
+
+gulp.task('minify-css', () => (
+  gulp.src(`${SOURCE}/**/*.css`)
+    .pipe(cleanCSS())
+    .pipe(gulp.dest(TARGET))
+));
+
+gulp.task('minify-html', () => (
+  gulp.src(`${SOURCE}/**/*.html`)
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest(TARGET))
+));
+
+gulp.task('transfer-resources', () => (
+  gulp.src(`${SOURCE}/resources/**/*`)
+    .pipe(gulp.dest(`${TARGET}/resources/`))
+));
+
+gulp.task('clean-target', () => {
+  del.sync(TARGET);
 });
 
-gulp.task('minify_css', () => {
-    return gulp.src(`${DIR_SOURCE}/**/*.css`)
-        .pipe(cleanCSS())
-        .pipe(gulp.dest(DIR_SOURCE));
-});
 
-gulp.task('minify_html', function() {
-    return gulp.src(`${DIR_SOURCE}/**/*.html`)
-        .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(gulp.dest(DIR_SOURCE));
-});
+/*
+https://webdesign-master.ru/blog/tools/2016-03-09-gulp-beginners.html
 
-
-// gulp.task('concat', function () {
-//     return gulp.src('public/css/dev/**/*.css')
-//         .pipe(concatCss("main.min.css"))
-//         .pipe(gulp.dest('public/css/concat/'));
-// });
-//
-// gulp.task('csso', function () {
-//     return gulp.src('public/css/concat/main.min.css')
-//         .pipe(csso())
-//         .pipe(gulp.dest('public/css/'));
-// });
-
-//
-// let gulp = require('gulp');
-// let cleanCSS = require('gulp-clean-css');
-//
-// gulp.task('minify-css', () => {
-//     return gulp.src('styles/*.css')
-//         .pipe(cleanCSS({compatibility: 'ie8'}))
-//         .pipe(gulp.dest('dist'));
-// });
+*/
