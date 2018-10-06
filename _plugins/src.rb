f056f7d968dbec5d3456659f3cc4f2f6b405c7ec
Evgeny Semenov <semenovem@gmail.com>
2018-10-06 18:26:05 +0300

    keep progress

--- _plugins/src.rb
+++ _plugins/src.rb
@@ -13,8 +13,9 @@ module Jekyll
     # add hash to the file name
     # @example file.css => file.css?81a568
     def add_hash(s)
+      t = Time.now()
       site = @context.registers[:site]
-      s + "?a" + site.config["short_hash_commit"].to_s.chomp("/")
+      s + "?a=" + (site.config["short_hash_commit"] ? site.config["short_hash_commit"].to_s.chomp("/") : t.to_i.to_s)
     end
 
 
--- js/a_base.js
+++ js/a_base.js
@@ -7,6 +7,7 @@
 
   function handleReadyStateChange() {
     if (document.readyState === 'complete') {
+      document.removeEventListener('readystatechange', handleReadyStateChange);
       stickFooter();
     }
   }
